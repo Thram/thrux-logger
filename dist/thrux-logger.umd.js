161a54127f9533b45f9414fada7fc18f96bdd1d3
Thram <thramposo@gmail.com>
2017-02-08 18:28:19 +1300

    fix(webpack): Update webpack and rebuild

--- dist/thrux-logger.umd.js
+++ dist/thrux-logger.umd.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["thrux"] = factory();
+		exports["thrux-logger"] = factory();
 	else
-		root["thrux"] = factory();
+		root["thrux-logger"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
