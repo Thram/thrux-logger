22f0ccad9f534f8aee419296d0fd01115ff545e9
Thram <thramposo@gmail.com>
2017-02-08 18:30:57 +1300

    fix(builds): Update builds

--- dist/thrux-logger.umd.js
+++ dist/thrux-logger.umd.js
@@ -21,16 +21,16 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -43,64 +43,90 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	/**
-	 * Created by thram on 27/01/17.
-	 */
-	
-	var colors = {
-	  black: 'color: black;',
-	  red: 'color: crimson;',
-	  green: 'color: teal;',
-	  blue: 'color: cornflowerblue;'
-	},
-	    fontWeights = {
-	  bold: 'font-weight: bold;',
-	  normal: 'font-weight: normal;'
-	};
-	
-	var blackTitle = colors.black + ' ' + fontWeights.bold,
-	    blackText = colors.black + ' ' + fontWeights.normal,
-	    greenTitle = colors.green + ' ' + fontWeights.bold,
-	    greenText = colors.green + ' ' + fontWeights.normal,
-	    redTitle = colors.red + ' ' + fontWeights.bold,
-	    redText = colors.red + ' ' + fontWeights.normal,
-	    blueTitle = colors.blue + ' ' + fontWeights.bold,
-	    blueText = colors.blue + ' ' + fontWeights.normal;
-	
-	var logger = function logger(_ref) {
-	  var state = _ref.state,
-	      action = _ref.action,
-	      prev = _ref.prev,
-	      payload = _ref.payload,
-	      next = _ref.next;
-	
-	  console[console.group ? 'group' : 'log']('%cState: %c' + state + ' - %cAction: %c' + action + ' - %cTime: %c' + new Date().toISOString(), greenText, greenTitle, blueText, blueTitle, blackText, blackTitle);
-	  console.log('%cPrevious State:', redTitle, prev);
-	  console.log('%cPayload:', blackTitle, payload);
-	  console.log('%cNext State:', blueTitle, next);
-	  console.group && console.groupEnd();
-	};
-	
-	exports.default = logger;
+"use strict";
 
-/***/ }
-/******/ ])
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-;
+/**
+ * Created by thram on 27/01/17.
+ */
+
+var colors = {
+  black: 'color: black;',
+  red: 'color: crimson;',
+  green: 'color: teal;',
+  blue: 'color: cornflowerblue;'
+},
+    fontWeights = {
+  bold: 'font-weight: bold;',
+  normal: 'font-weight: normal;'
+};
+
+var blackTitle = colors.black + ' ' + fontWeights.bold,
+    blackText = colors.black + ' ' + fontWeights.normal,
+    greenTitle = colors.green + ' ' + fontWeights.bold,
+    greenText = colors.green + ' ' + fontWeights.normal,
+    redTitle = colors.red + ' ' + fontWeights.bold,
+    redText = colors.red + ' ' + fontWeights.normal,
+    blueTitle = colors.blue + ' ' + fontWeights.bold,
+    blueText = colors.blue + ' ' + fontWeights.normal;
+
+var logger = function logger(_ref) {
+  var state = _ref.state,
+      action = _ref.action,
+      prev = _ref.prev,
+      payload = _ref.payload,
+      next = _ref.next;
+
+  console[console.group ? 'group' : 'log']('%cState: %c' + state + ' - %cAction: %c' + action + ' - %cTime: %c' + new Date().toISOString(), greenText, greenTitle, blueText, blueTitle, blackText, blackTitle);
+  console.log('%cPrevious State:', redTitle, prev);
+  console.log('%cPayload:', blackTitle, payload);
+  console.log('%cNext State:', blueTitle, next);
+  console.group && console.groupEnd();
+};
+
+exports.default = logger;
+
+/***/ })
+/******/ ]);
+});
 //# sourceMappingURL=thrux-logger.umd.js.map
